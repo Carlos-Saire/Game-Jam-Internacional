629bf5a46ad2bee44d5ac45396cb95ae91015bfc
victorcollazosisnull <victor.collazos@tecsup.edu.pe>
2025-10-18 18:40:43 -0500

    Level3

---
+++ Assets/ScriptsLevel3/Candy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+namespace Game3
+{
+    public class Candy : ClickableItem
+    {
+        protected override void Start()
+        {
+            base.Start();
+            moveSpeed = 3.5f; 
+        }
+
+        public override void OnClick()
+        {
+            Debug.Log("Click caramelo");
+            GameManagerLevel3.Instance.AddScore(1);
+            // SE PUEDE PONER EL SONIDO MALO ACA O SUS EFECTOS VISUALES si es q hay xd
+            Destroy(gameObject);
+        }
+    }
+}
