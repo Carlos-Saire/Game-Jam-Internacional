de37982f351bb7e9a41901d20914b1bbc6121b73
Chacashi <eljohndelapuna@gmail.com>
2025-10-19 09:16:15 -0500

    tengo sueño

--- Assets/ScriptsNivel2/EnemyController.cs
+++ Assets/ScriptsNivel2/EnemyController.cs
@@ -5,30 +5,39 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class EnemyController : StartableEntity
 {
     Rigidbody2D _compRigidbody2D;
     [SerializeField] float TimeFreeze;
     [SerializeField] float MaxTimeFreeze;
-    [SerializeField] TMP_Text textTimeFreeze;
+    [SerializeField] Vector2 FirstDestination;
+    [SerializeField] Sprite ScarySprite;
+    [SerializeField] Sprite NormalSprite;
     Vector2 PositionToMove;
     [SerializeField] Vector3 startPostition;
     [SerializeField] float speedMove;
     public static event Action OnTimeisOver;
     public static event Action OnCreateTrush;
-    bool isTimeZero;
+
     [SerializeField] GameObject trashPrefab;
-    int numberMagic;
+    [SerializeField] private bool stateFlipInitial;
+
+    private SpriteRenderer sprite;
+    private bool isReturning = false;
+    private bool hasReachedInitialPosition = false;
+
     private void Awake()
     {
         _compRigidbody2D = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     private void Start()
     {
         transform.position = startPostition;
-        SetTextTimeFreeze(TimeFreeze);
+        PositionToMove = FirstDestination;
+        hasReachedInitialPosition = false;
+        isReturning = false;
         ActivateEnemys();
     }
 
@@ -41,54 +50,67 @@ public class EnemyController : StartableEntity
     protected override void OnDisable()
     {
         base.OnDisable();
-        InputReader.OnClickLeft += ScaryGhost;
+        InputReader.OnClickLeft -= ScaryGhost;
     }
+
     private void Update()
     {
         if (!isStartGame) return;
 
         if (TimeFreeze > 0)
         {
-            transform.position = Vector2.MoveTowards(transform.position, startPostition, speedMove * Time.deltaTime);
-            if (transform.position == startPostition)
+            isReturning = true;
+            MoveTo(startPostition);
+
+            if (transform.position.x < startPostition.x)
+                sprite.flipX = true;
+            else if (transform.position.x > startPostition.x)
+                sprite.flipX = false;
+
+            if (Vector2.Distance(transform.position, startPostition) < 0.01f)
             {
-                transform.GetComponent<BoxCollider2D>().enabled = false;
                 TimeFreeze -= Time.deltaTime;
-                SetTextTimeFreeze(Mathf.FloorToInt(TimeFreeze));
+                GetComponent<BoxCollider2D>().enabled = false;
+                sprite.flipX = stateFlipInitial;
+                sprite.sprite = NormalSprite;
+                hasReachedInitialPosition = true;
             }
-
         }
-        if (TimeFreeze < 0)
+        else if (TimeFreeze <= 0 && hasReachedInitialPosition)
         {
-            transform.GetComponent<BoxCollider2D>().enabled = true;
-            isTimeZero = true;
-            SetTextTimeFreeze(0);
-            transform.position = Vector2.MoveTowards(transform.position, PositionToMove, speedMove * Time.deltaTime);
+            isReturning = false;
+            GetComponent<BoxCollider2D>().enabled = true;
         }
 
+        if (!isReturning)
+        {
+            MoveTo(PositionToMove);
+        }
+    }
 
-
+    void MoveTo(Vector2 destination)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destination, speedMove * Time.deltaTime);
     }
 
     void ActivateEnemys()
     {
-        if (isTimeZero)
+        if (TimeFreeze <= 0 && hasReachedInitialPosition)
         {
             OnTimeisOver?.Invoke();
         }
     }
 
-    void SetTextTimeFreeze(float time)
-    {
-        textTimeFreeze.text = time.ToString();
-    }
-
     public void SetNewPosition(Vector2 newPosition)
     {
+        // Flip sprite based on direction
+        if (transform.position.x < newPosition.x)
+            sprite.flipX = true;
+        else if (transform.position.x > newPosition.x)
+            sprite.flipX = false;
+
         PositionToMove = newPosition;
     }
-
-
 
     void ScaryGhost()
     {
@@ -96,30 +118,34 @@ public class EnemyController : StartableEntity
         RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
         if (hit.collider != null && hit.collider.CompareTag("Ghost"))
         {
-           
-                TimeFreeze = MaxTimeFreeze;   
+            TimeFreeze = MaxTimeFreeze;
+            sprite.sprite = ScarySprite;
+            hasReachedInitialPosition = false;
         }
     }
 
-   
-
-    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Node")
+
+        if (collision.CompareTag("Node") )
         {
 
-            numberMagic = UnityEngine.Random.Range(0, 101);
+
+            int numberMagic = UnityEngine.Random.Range(0, 101);
             if (numberMagic <= 17)
             {
                 OnCreateTrush?.Invoke();
                 Instantiate(trashPrefab, transform.position, Quaternion.identity);
             }
-            SetNewPosition(collision.GetComponent<NodeControll>().GetAdjacentNode().transform.position);
 
+
+            NodeControll currentNode = collision.GetComponent<NodeControll>();
+            NodeControll nextNode = currentNode.GetAdjacentNode();
+
+            if (nextNode != null)
+            {
+                SetNewPosition(nextNode.transform.position);
+            }
         }
     }
-
-   
-
 }
